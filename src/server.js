65c03e12f821f435dd0646b4dec8edc40969ce17
Nakdimon <gjherrera.i@gmail.com>
2019-07-14 21:44:14 -0600

    Adjusting webpack configurations
    Removed Webpack html plugin.
    Added package-lock, package and dist to gitignore.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import path from 'path'
 import express from 'express'
 const app = express(),
-            DIST_DIR = __dirname,
-            HTML_FILE = path.join(DIST_DIR, 'index.html')
+            DIST_DIR = __dirname            
 app.use(express.static(DIST_DIR))
-app.get('*', (req, res) => {
-    res.sendFile(HTML_FILE)
-})
+
+app.get('/', function (req, res) {
+ return res.send('Hello world');
+});
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path')
 const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
   entry: {
-    server: './server.js',
+    server: './src/server.js',
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist/src'),
     publicPath: '/',
     filename: '[name].js'
   },
@@ -27,20 +26,7 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      },
-      {
-        // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
-        test: /\.html$/,
-        use: [{loader: "html-loader"}]
-      }
+      }      
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./index.html",
-      filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    })
-  ]
+  }
 }
